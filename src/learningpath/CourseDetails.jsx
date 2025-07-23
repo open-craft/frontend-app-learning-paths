8882f8a6be18c2b5b17442bb68b69aea460591a1
Agrendalath <piotr@surowiec.it>
2025-07-23 13:29:09 +0200

    feat: mobile designs for details pages

--- src/learningpath/CourseDetails.jsx
+++ src/learningpath/CourseDetails.jsx
@@ -81,7 +81,10 @@ const CourseDetailContent = ({
             </ModalCloseButton>
           </Row>
         )}
-        <Card orientation="horizontal">
+        <Card orientation={isSmall ? 'vertical' : 'horizontal'}>
+          {isSmall && (
+            <Card.ImageCap src={buildAssetUrl(courseImageAssetPath)} logoSrc={orgData.logo} className="mb-4" />
+          )}
           <Card.Body>
             {!isModalView && (
             <Card.Section>
@@ -100,39 +103,41 @@ const CourseDetailContent = ({
               </p>
             </Card.Section>
           </Card.Body>
-          <Card.ImageCap src={buildAssetUrl(courseImageAssetPath)} logoSrc={orgData.logo} />
+          {!isSmall && (
+            <Card.ImageCap src={buildAssetUrl(courseImageAssetPath)} logoSrc={orgData.logo} />
+          )}
         </Card>
-        <Row className="my-4 mx-0 px-6 d-flex hero-info course-hero-info">
+        <Row className="my-4 mx-0 px-5 px-md-6 flex-column flex-md-row align-items-start hero-info course-hero-info">
           {dateDisplay && (
             <div className="d-flex align-items-center">
               <Icon src={AccessTimeFilled} className="mr-4 mb-3" />
               <div>
-                <p className="mb-1 font-weight-bold">{dateDisplay}</p>
-                <p className="text-muted">Access ends</p>
+                <p className="mb-0 font-weight-bold">{dateDisplay}</p>
+                <p className="mb-2 text-muted">Access ends</p>
               </div>
             </div>
           )}
           <div className="d-flex align-items-center">
             <Icon src={Award} className="mr-4 mb-4" />
             <div>
-              <p className="mb-1 font-weight-bold">Certificate</p>
-              <p className="text-muted">Earn a certificate</p>
+              <p className="mb-0 font-weight-bold">Certificate</p>
+              <p className="mb-2 text-muted">Earn a certificate</p>
             </div>
           </div>
           {duration && (
             <div className="d-flex align-items-center">
               <Icon src={Calendar} className="mr-4 mb-4" />
               <div>
-                <p className="mb-1 font-weight-bold">{duration}</p>
-                <p className="text-muted">Approx. duration</p>
+                <p className="mb-0 font-weight-bold">{duration}</p>
+                <p className="mb-2 text-muted">Approx. duration</p>
               </div>
             </div>
           )}
           <div className="d-flex align-items-center">
             <Icon src={Person} className="mr-4 mb-4" />
             <div>
-              <p className="mb-1 font-weight-bold">{selfPaced ? 'Self-paced' : 'Instructor-paced'}</p>
-              <p className="text-muted">
+              <p className="mb-0 font-weight-bold">{selfPaced ? 'Self-paced' : 'Instructor-paced'}</p>
+              <p className="mb-2 text-muted">
                 {selfPaced ? 'Progress at your own speed' : 'Follow the course schedule'}
               </p>
             </div>
